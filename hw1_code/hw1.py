4ff4e185de60c36a4478e5187f3250afd0428784
rocky1991 <hongchuan91@gmail.com>
2020-09-21 19:16:58 -0600

    finished association rule

--- hw1_code/hw1.py
+++ hw1_code/hw1.py
@@ -1,6 +1,7 @@
 import itertools
 
-MINSUP = 20
+MINSUP = 15
+MINCONF = 0.01
 
 def read_csv(file_path):
 	colnames = ""
@@ -41,12 +42,13 @@ def load_tranc_db(file_path):
 ############   input: dataframe df, unique_depts, minsup
 ############   output: Frequent item set
 class Node:
-	def __init__(self, dept_name = {}, sup = 0, parent = "", children = []):
+	def __init__(self, dept_name = set(), sup = 0, parent = "", children = [],c=0,rsup=0):
 		self.dept_name = dept_name
 		self.sup = sup
 		self.children = children
 		self.parent = parent
-
+		self.c=c
+		self.rsup=rsup
 	def set_dept_name(self, name):
 		self.dept_name = name 
 	def set_sup(self,sup):
@@ -84,7 +86,7 @@ D = load_tranc_db("transaction_db.txt")
 F = []
 
 root_node = Node(dept_name = {"root_node"})
-root_node.print_children()
+# root_node.print_children()
 
 unique_level1_nodes = []
 for unique_dept in unique_depts:
@@ -140,15 +142,98 @@ while (len(Ck) != 0):
 
 	k = k + 1
 
-for node in F:
-	print(node.sup)
+# for node in F:
+# 	print(node.dept_name)
 
 
 
+def find_support(dept_name, Frequent_set):
+	for node in Frequent_set:
+		if(len(node.dept_name.intersection(dept_name)) == len(dept_name)):
+			return node.sup
+
+def find_max_elem_len(setOfSets):
+	max_elem_len = 0
+	for aSet in setOfSets:
+		if len(aSet) > max_elem_len:
+			max_elem_len = len(aSet)
+	return max_elem_len
+
+class Rule:
+	def __init__(self, X = set(), Y = set(), sup_Z = 0, c = 0, rsup=0, lift=0):
+		self.X = X
+		self.Y = Y
+		self.sup_Z = sup_Z
+		self.c = c
+		self.rsup=rsup
+		self.lift=lift
+
+	def set_X(self, X):
+		self.X = X
+	def set_Y(self, Y):
+		self.Y = Y
+	def set_sup_Z(self, sup_Z):
+		self.sup_Z = sup_Z
+	def set_c(self, c):
+		self.c = c
+	def set_rsup(self, rsup):
+		self.rsup = rsup
+	def set_lift(self, lift):
+		self.lift = lift
 
 
 # generate set of strong rules
+rules = []
+# def associationRules(F,minconf):
+for Z in F:
+
+	if (len(Z.dept_name)>=2):
+		# A = set(itertools.combinations(Z.dept_name))
+		A = set()
+		num_sets = len(Z.dept_name)
+		
+		for i in range(1,len(Z.dept_name)):
+			tempset=[]
+			tempset=set(itertools.combinations(Z.dept_name,i))
+			A = A | (tempset)
+		# print("The Z length is " + str(len(Z.dept_name)))
+		while (len(A) > 0):
+
+			# find the maximal element in A
+			max_elem = set()
+			max_elem_len = find_max_elem_len(A)
+			for aSet in A:
+				if (len(aSet) == max_elem_len):
+					max_elem = aSet
+					break
+			# print("Z dept name is ") 
+			# print(Z.dept_name)
+			# print("max_elem is ")
+			# print(max_elem)
+			# print("A is ")
+			# print(A)
+			A.remove(max_elem)
+			if (len(A) == 0):
+				continue
+			c = Z.sup / find_support(max_elem, F)
+		
+			if (c > MINCONF):
+				newRule = Rule(X = set(max_elem).copy(), Y = A.copy(), sup_Z = Z.sup, c = c)
+				rules.append(newRule)
+			else:
+				if (len(max_elem) > 1):
+					for i in range(1,len(max_elem)):
+						tempset=[]
+						tempset=set(itertools.combinations(max_elem,i))
+						for aSet in tempset:
+							A.remove(aSet)
+			# print("c value is " + str(c))
 
 
-# rank rules 
+for rule in rules:
+	print("This is a new rule >>>>>>>>>>>>>>>>>>>")
+	print("X is " + str(rule.X) + " and Y is " + str(rule.Y))
+	print("sup(Z) is " + str(rule.sup_Z))
+	print("c is " + str(rule.c))
 
+#print(str())
